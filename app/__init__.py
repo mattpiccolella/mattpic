4ebd0436f9674e9cb4b065840c57c50eecc32a48
Matthew Piccolella <mjp2220@columbia.edu>
2015-06-10 07:38:36 +0000

    Simple landing page

--- app/__init__.py
+++ app/__init__.py
@@ -12,14 +12,6 @@ app.register_blueprint(blog_module)
 assets = Environment(app)
 assets.url = app.static_url_path
 
-# Compile SCSS to CSS
-scss = Bundle('scss/main.scss', depends='scss/*/*.scss', filters='pyscss', output='css/main.css')
-assets.register('scss_main', scss)
-
-# Compile JavaScript to minified version.
-js = Bundle('js/base.js', 'js/jquery.js', filters='jsmin', output='js/main.js')
-assets.register('js_main', js)
-
 @app.route("/")
 def home():
   return render_template("index.html")
