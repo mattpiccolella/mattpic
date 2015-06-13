5900e264b70d0f0d5c94b7fd0d4458e494c32233
Matthew Piccolella <mjp2220@columbia.edu>
2015-06-13 19:20:23 +0000

    Got the block working

--- app/blog/controllers.py
+++ app/blog/controllers.py
@@ -1,7 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, Flask, render_template
+from flask_flatpages import FlatPages, pygments_style_defs
+from flask_frozen import Freezer
+from .. import app
+import sys, os
 
 blog = Blueprint('blog', __name__, url_prefix='/blog')
+flatpages = FlatPages(app)
+
 
 @blog.route("/")
-def hello():
-  return "This is my first blog!"
+def list():
+  posts = [p for p in flatpages]
+  posts.sort(key=lambda item:item['date'], reverse=False)
+  return render_template('blog.html', posts=posts)
+
+@blog.route('/<name>')
+def post(name):
+  post = flatpages.get_or_404(name)
+  return render_template('blog_post.html', post=post)
--- config/app_config.py
+++ config/app_config.py
@@ -4,3 +4,8 @@ DEBUG = True
 # Define the application directory
 import os
 BASE_DIR = os.path.abspath(os.getcwd())
+
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
+FLATPAGES_ROOT = 'blog/content'
+POST_DIR = 'posts'
